c3a0a4f1103147f62d6502098d9481889df65d70
Jason Goldberger <jlgoldb2@asu.edu>
2018-02-28 15:40:11 -0700

    added periodic_task_test.exs

---
+++ test/api_checker/periodic_task_test.exs
@@ -0,0 +1,50 @@
+defmodule ApiChecker.PeriodicTaskTest do
+  use ExUnit.Case
+  alias ApiChecker.{PeriodicTask, ApiValidator}
+  doctest ApiChecker.PeriodicTask
+
+  @valid_periodic_task_json %{
+    "name" => "mbta-testing-01",
+    "url" => "https://api-v3.mbta.com/predictions?filter%5Broute%5D=Red,Orange,Blue",
+    "active" => true,
+    "frequency_in_seconds" => 120,
+    "time_ranges" => [
+      %{"type" => "weekly", "day" => "MON", "start" => "06:30", "stop" => "22:00"},
+      %{"type" => "weekly", "day" => "TUE", "start" => "06:30", "stop" => "22:00"},
+      %{"type" => "weekly", "day" => "WED", "start" => "06:30", "stop" => "22:00"},
+      %{"type" => "weekly", "day" => "THU", "start" => "06:30", "stop" => "22:00"},
+      %{"type" => "weekly", "day" => "FRI", "start" => "06:30", "stop" => "22:00"}
+    ],
+    "validators" => [
+      %{"keypath" => ["data"], "validator" => ["array", "not_empty"]},
+      %{"keypath" => ["jsonapi"], "validator" => "jsonapi"}
+    ]
+  }
+  describe "from_json/1" do
+    test "works for valid json" do
+      assert {:ok, task} = PeriodicTask.from_json(@valid_periodic_task_json)
+
+      assert task.validators == [
+               %ApiValidator{keypath: ["data"], validator: ["array", "not_empty"]},
+               %ApiValidator{keypath: ["jsonapi"], validator: "jsonapi"}
+             ]
+
+      assert task.time_ranges == [
+               %ApiChecker.PeriodicTask.WeeklyTimeRange{day: "MON", start: ~T[06:30:00], stop: ~T[22:00:00]},
+               %ApiChecker.PeriodicTask.WeeklyTimeRange{day: "TUE", start: ~T[06:30:00], stop: ~T[22:00:00]},
+               %ApiChecker.PeriodicTask.WeeklyTimeRange{day: "WED", start: ~T[06:30:00], stop: ~T[22:00:00]},
+               %ApiChecker.PeriodicTask.WeeklyTimeRange{day: "THU", start: ~T[06:30:00], stop: ~T[22:00:00]},
+               %ApiChecker.PeriodicTask.WeeklyTimeRange{day: "FRI", start: ~T[06:30:00], stop: ~T[22:00:00]}
+             ]
+
+      assert task.name == "mbta-testing-01"
+      assert task.url == "https://api-v3.mbta.com/predictions?filter%5Broute%5D=Red,Orange,Blue"
+      assert task.active == true
+      assert task.frequency_in_seconds == 120
+    end
+
+    test "errors for invalid json" do
+      assert {:error, _} = @valid_periodic_task_json |> Map.drop(["validators"]) |> PeriodicTask.from_json()
+    end
+  end
+end
