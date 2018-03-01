e06c8331dd5405bc6f5b7c1f91e63aef32e5de54
Sebastian Abondano <sabondano1@gmail.com>
2018-03-01 17:35:27 -0500

    Remove unused alias in

--- lib/api_checker/periodic_task.ex
+++ lib/api_checker/periodic_task.ex
@@ -3,7 +3,7 @@ defmodule ApiChecker.PeriodicTask do
   Provides context, parsing, validating for configuration of
   a PeriodicTask with the intention of configuring a worker process.
   """
-  alias ApiChecker.{PeriodicTask, JsonCheck, PreviousResponse}
+  alias ApiChecker.{PeriodicTask, JsonCheck}
   alias ApiChecker.PeriodicTask.{WeeklyTimeRange, Times}
 
   defstruct frequency_in_seconds: nil,
