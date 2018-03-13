0f8a187def34d01f1db401ec3f2b846881453c1d
Paul Swartz <paul@paulswartz.net>
2018-03-13 14:10:18 -0400

    update StaleDataCheck to use `modified_at`

--- test/api_checker/check/state_data_check_test.exs
+++ test/api_checker/check/state_data_check_test.exs
@@ -1,4 +1,5 @@
 defmodule ApiChecker.Check.StaleDataCheckTest do
+  @moduledoc false
   use ExUnit.Case, async: true
   alias ApiChecker.Check.{StaleDataCheck, Params}
   alias ApiChecker.PreviousResponse
@@ -6,24 +7,24 @@ defmodule ApiChecker.Check.StaleDataCheckTest do
 
   describe "is_old_enough_to_be_stale?/3" do
     test "true for diff that is greater than the limit" do
-      updated_at = datetime("2018-02-28T19:00:00Z")
+      modified_at = datetime("2018-02-28T19:00:00Z")
       at_time = datetime("2018-02-28T19:05:00Z")
       # 1 second
       limit = 1
-      assert StaleDataCheck.is_old_enough_to_be_stale?(limit, updated_at, at_time)
+      assert StaleDataCheck.is_old_enough_to_be_stale?(limit, modified_at, at_time)
     end
 
     test "false for diff that is less than the limit" do
-      updated_at = datetime("2018-02-28T19:00:00Z")
+      modified_at = datetime("2018-02-28T19:00:00Z")
       at_time = datetime("2018-02-28T19:05:00Z")
       # 10 minutes
       limit = 600
-      refute StaleDataCheck.is_old_enough_to_be_stale?(limit, updated_at, at_time)
+      refute StaleDataCheck.is_old_enough_to_be_stale?(limit, modified_at, at_time)
     end
 
     test "works for StaleDataCheck struct and PreviousResponse struct" do
       stale_check = %StaleDataCheck{time_limit_in_seconds: 1}
-      prev = %PreviousResponse{updated_at: datetime("2018-02-28T19:00:00Z")}
+      prev = %PreviousResponse{modified_at: datetime("2018-02-28T19:00:00Z")}
       at_time = datetime("2018-02-28T19:05:00Z")
       assert StaleDataCheck.is_old_enough_to_be_stale?(stale_check, prev, at_time)
     end
@@ -46,7 +47,7 @@ defmodule ApiChecker.Check.StaleDataCheckTest do
       params = %Params{
         raw_body: "123",
         previous_response: %PreviousResponse{
-          updated_at: datetime("2018-02-28T19:00:00Z"),
+          modified_at: datetime("2018-02-28T19:00:00Z"),
           body: "123"
         },
         check_time: datetime("2018-02-28T19:05:00Z")
@@ -61,7 +62,7 @@ defmodule ApiChecker.Check.StaleDataCheckTest do
       params = %Params{
         raw_body: "123",
         previous_response: %PreviousResponse{
-          updated_at: datetime("2018-02-28T19:00:00Z"),
+          modified_at: datetime("2018-02-28T19:00:00Z"),
           body: "123"
         },
         check_time: datetime("2018-02-28T19:05:00Z")
@@ -76,7 +77,7 @@ defmodule ApiChecker.Check.StaleDataCheckTest do
       params = %Params{
         raw_body: "123",
         previous_response: %PreviousResponse{
-          updated_at: datetime("2018-02-28T19:00:00Z"),
+          modified_at: datetime("2018-02-28T19:00:00Z"),
           body: "something different"
         },
         check_time: datetime("2018-02-28T19:05:00Z")
