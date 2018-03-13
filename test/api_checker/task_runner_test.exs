a35d1652ad4828bf90e7ed7c252406371c8c1c65
Paul Swartz <paul@paulswartz.net>
2018-03-13 13:49:05 -0400

    factor out building an updated PreviousResponse

--- test/api_checker/task_runner_test.exs
+++ test/api_checker/task_runner_test.exs
@@ -1,6 +1,6 @@
 defmodule ApiChecker.TaskRunnerTest do
   use ExUnit.Case, async: true
-  alias ApiChecker.{TaskRunner, PeriodicTask}
+  alias ApiChecker.{TaskRunner, PeriodicTask, PreviousResponse}
   alias ApiChecker.Check.JsonCheck
   import ExUnit.CaptureLog
 
@@ -22,10 +22,8 @@ defmodule ApiChecker.TaskRunnerTest do
   }
 
   describe "perform/1" do
-    # previous response is passed as %{}.
-
     test "can run one task with multiple checks" do
-      captured = capture_log(fn -> TaskRunner.perform(@valid_periodic_task, %{}) end)
+      captured = capture_log(fn -> TaskRunner.perform(@valid_periodic_task, %PreviousResponse{}) end)
       assert captured =~ ~s(Check OK)
       assert captured =~ ~s(task_name="mbta-testing-01")
       assert captured =~ ~s(%ApiChecker.Check.JsonCheck{expects: "not_empty", keypath: ["data"]})
@@ -33,7 +31,7 @@ defmodule ApiChecker.TaskRunnerTest do
     end
 
     test "logs failure for failed check" do
-      captured = capture_log(fn -> TaskRunner.perform(@failure_periodic_task, %{}) end)
+      captured = capture_log(fn -> TaskRunner.perform(@failure_periodic_task, %PreviousResponse{}) end)
       assert captured =~ ~s(Check Failure)
       assert captured =~ ~s(task_name="failure-task")
       assert captured =~ ~s(%ApiChecker.Check.JsonCheck{expects: "not_empty", keypath: ["unexpected"]})
@@ -42,7 +40,9 @@ defmodule ApiChecker.TaskRunnerTest do
   end
 
   test "can run one multiple tasks" do
-    captured = capture_log(fn -> TaskRunner.perform([@valid_periodic_task, @failure_periodic_task], %{}) end)
+    captured =
+      capture_log(fn -> TaskRunner.perform([@valid_periodic_task, @failure_periodic_task], %PreviousResponse{}) end)
+
     assert captured =~ ~s(Check OK - task_name="mbta-testing-01")
     assert captured =~ ~s(Check Failure - task_name="failure-task")
   end
