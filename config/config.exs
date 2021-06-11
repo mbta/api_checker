05efa36817237df4d23dc7a0cc1355970c2db39c
Paul Swartz <paul@paulswartz.net>
2021-06-11 14:11:54 -0400

    refactor: don't use `Mix.env()` in the application
    Instead, configure the check filename in config and read it that way.

--- config/config.exs
+++ config/config.exs
@@ -1,5 +1,10 @@
 use Mix.Config
 
-if Mix.env() == :test do
-  config :logger, backends: []
+case Mix.env() do
+  :dev ->
+    config :api_checker, check_filename: "priv/dev_checks_config.json"
+
+  :test ->
+    config :logger, backends: []
+    config :api_checker, check_filename: "priv/test_checks_config.json"
 end
